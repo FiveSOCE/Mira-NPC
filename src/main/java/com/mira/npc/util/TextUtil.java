6aca3309eddbf61ea8bc9eb51d49860c9fc9728e
FiveS <sgmmsionide@gmail.com>
2026-09-03 05:48:56 +1000

    Add MiraNPC text utilities

---
+++ src/main/java/com/mira/npc/util/TextUtil.java
@@ -0,0 +1,14 @@
+package com.mira.npc.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+public final class TextUtil {
+    private static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.legacyAmpersand();
+
+    private TextUtil() {}
+
+    public static Component component(String text) {
+        return LEGACY.deserialize(text == null ? "" : text);
+    }
+}
