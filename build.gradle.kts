4934bab8fc959a24183873330af28c6a1b4d0307
FiveS <sgmmsionide@gmail.com>
2026-09-09 06:02:15 +1000

    Add 1.21.11-26.2 compatibility build for MiraNPC 0.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,23 @@ plugins {
 }
 
 group = "com.mira"
-version = "0.1.4"
+version = "0.1.5"
 
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
 }
 
+val paperApiVersion = providers.gradleProperty("paperApiVersion").orElse("1.21.11-R0.1-SNAPSHOT")
+val compileJavaVersion = providers.gradleProperty("compileJavaVersion").map(String::toInt).orElse(21)
+val bytecodeJavaVersion = providers.gradleProperty("bytecodeJavaVersion").map(String::toInt).orElse(21)
+
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:${paperApiVersion.get()}")
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(compileJavaVersion.get()))
 }
 
 tasks.withType<JavaCompile>().configureEach {
